e8b0e23750bc461e1c59ce81cea5c6c5ca5ddf80
Eric-IIP <s2465062@u.tsukuba.ac.jp>
2025-12-26 21:45:16 +0900

    first commit

---
+++ neuralnet.py
@@ -0,0 +1,21 @@
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+
+class SCTNet(nn.Module):
+    def __init__(self, input_dim):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(64, 1)
+        )
+
+    def forward(self, x):
+        return self.net(x)
